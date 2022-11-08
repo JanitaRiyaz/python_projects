f2c6a26476f2016f20b69fa2a7ec377dc30e3ee2
janita <janitariyaz124@gmail.com>
2022-11-08 17:32:11 +0500

    Create Madlibs

---
+++ Madlibs.py
@@ -0,0 +1,12 @@
+adj=input('Adjective: ')
+verb1=input('Verb: ')
+verb2=input('verb: ')
+adj2=input('Adjective: ')
+famous_person=input('famous person: ')
+
+
+
+
+
+madlibs=f'Programming is so {adj}. Whenenver i learn programming i fell {verb1} and {verb2} Stay hydrated and {adj2} like you are {famous_person}'
+print(madlibs)
